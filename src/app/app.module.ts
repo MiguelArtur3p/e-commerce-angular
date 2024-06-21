ceb8fd9e0bcc6c5f6cb47ec0c8ba286e50763878
MiguelArtur3p <miguel.3tp@hotmail.com>
2024-06-21 20:51:47 -0300

    default header created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultHeaderComponent } from './default-layout/default-header/default-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultHeaderComponent
   ],
   imports: [
     BrowserModule,
